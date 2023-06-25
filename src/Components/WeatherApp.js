b0a5097e696f61e1aff283447264ef979bb21aa8
amitkumar-developer <itsamitkumar03@gmail.com>
2023-06-25 11:54:29 +0530

    bg

--- src/Components/WeatherApp.js
+++ src/Components/WeatherApp.js
@@ -29,15 +29,21 @@ const WeatherApp = () => {
     )
       .then((response) => response.json())
       .then((data) => {
+        if (data.cod && data.cod !== 200) {
+          alert('Invalid location. Please enter a valid city name.');
+          return;
+        }
         setWeatherData(data);
- 
       })
-   
+    
   };
-
+  
   return (
-    <div className="min-h-screen bg-pink-200 flex items-center justify-center">
-      <div className="bg-red-400 shadow-lg rounded-lg p-6">
+    <div className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center sm:bg-contain sm:bg-top" style={{ backgroundImage: 'url("https://ane4bf-datap1.s3-eu-west-1.amazonaws.com/wmocms/s3fs-public/styles/featured_media_detail/public/23613146140_6edfea579f_k.jpg?m0oNCnmjJAswk0CJrWZ.iW.ge6XbrfR1&itok=YBQjaq77")' }}>
+
+    {/* // <div className="min-h-screen bg-pink-200 flex items-center justify-center"> */}
+    {/* // <div className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center sm:bg-contain sm:bg-top" style={{ backgroundImage: 'url("https://img.freepik.com/free-vector/watercolor-weather-effects-collection_23-2149115331.jpg?w=2000")' }}> */}
+      <div className="bg-red-400 shadow-lg rounded-lg p-6" style={{ backgroundImage:'url("https://images1.livehindustan.com/uploadimage/library/2022/07/02/16_9/16_9_1/pti06_30_2022_000082b_0_jpg_1656770465.jpg")'}}>
         <h2 className=" text-2xl mb-4">Weather App</h2>
         <form onSubmit={handleFormSubmit} >
           <input 
@@ -56,7 +62,8 @@ const WeatherApp = () => {
         </form>
         
         {weatherData && (
-          <div className="bg-pink-600">
+          <div >
+
             <h3 className=" text-xl mb-2">{weatherData.name}</h3>
             <p className="mb-2">
               Temperature: {weatherData.main.temp} Kelvin
